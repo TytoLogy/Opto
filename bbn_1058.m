40e186a120659fba48e5349efe8e2f8c5d395bbd
Sharad Shanbhag <sshanbhag@neomed.edu>
2016-06-23 17:26:45 -0400

    new test scripts

---
+++ bbn_1058.m
@@ -0,0 +1,55 @@
+%-------------------------------------------------------------------------
+% Set Type of test
+%-------------------------------------------------------------------------
+% 	'LEVEL'			firing rate vs. stimulus level (dB)
+%	`FREQ'			frequency-tuning curve (tones)
+%	'FRA'				frequency-response area (tones)
+% 	'OPTO'			simple optical stimulation (nothing varies)
+% 	'OPTO-xxx'		optical stim, with 'xxx' as variable, where 'xxx' is
+% 		'DELAY'			opto stim delay 
+% 		'DUR'				opto stim duration
+% 		'AMP'				opto stim amplitude
+% 						or some combination of these
+%-------------------------------------------------------------------------
+test.Type = 'LEVEL';
+
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+% OPTICAL settings
+%-------------------------------------------------------------------------
+test.opto.Enable = 0;
+test.opto.Delay = 0;
+test.opto.Dur = 100;
+test.opto.Amp = 50;
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+% Auditory stimulus settings
+%-------------------------------------------------------------------------
+
+% signal
+test.audio.signal.Type = 'noise';
+test.audio.signal.Fmin = 4000;
+test.audio.signal.Fmax = 80000;
+test.audio.Delay = 100;
+test.audio.Duration = 100;
+test.audio.Level = 50:5:80;
+test.audio.Ramp = 5;
+test.audio.Frozen = 0;
+test.audio.ISI = 300;
+
+test.Reps = 10;
+test.Randomize = 1;
+
+test.saveStim = 0;
+
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+% TDT
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+test.AcqDuration = 300;
+test.SweepPeriod = 301;
+
+ 
+
+
---
+++ freq_1058.m
@@ -0,0 +1,55 @@
+%-------------------------------------------------------------------------
+% Set Type of test
+%-------------------------------------------------------------------------
+% 	'LEVEL'			firing rate vs. stimulus level (dB)
+%	`FREQ'			frequency-tuning curve (tones)
+%	'FRA'				frequency-response area (tones)
+% 	'OPTO'			simple optical stimulation (nothing varies)
+% 	'OPTO-xxx'		optical stim, with 'xxx' as variable, where 'xxx' is
+% 		'DELAY'			opto stim delay 
+% 		'DUR'				opto stim duration
+% 		'AMP'				opto stim amplitude
+% 						or some combination of these
+%-------------------------------------------------------------------------
+test.Type = 'FREQ';
+
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+% OPTICAL settings
+%-------------------------------------------------------------------------
+test.opto.Enable = 0;
+test.opto.Delay = 100;
+test.opto.Dur = 200;
+test.opto.Amp = 500; % mV
+
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+% Auditory stimulus settings
+%-------------------------------------------------------------------------
+% signal
+test.audio.signal.Type = 'tone';
+test.audio.signal.Frequency = 5000:2500:80000;
+test.audio.signal.RadVary = 0;
+test.audio.Delay = 100;
+test.audio.Duration = 100;
+test.audio.Level = 80;
+test.audio.Ramp = 5;
+test.audio.Frozen = 0;
+test.audio.ISI = 200;
+
+test.Reps = 10;
+test.Randomize = 1;
+
+test.saveStim = 0;
+
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+% TDT
+%-------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+test.AcqDuration = 300;
+test.SweepPeriod = test.AcqDuration + 1;
+
+ 
+
+
