e06b0f87d360df9dc35b18cb136a7c453e11c2ab
Sharad Shanbhag <sshanbhag@neomed.edu>
2019-04-29 17:31:57 -0400

    continuing work

--- Tests/MTwav_procscript.m
+++ Tests/MTwav_procscript.m
@@ -52,11 +52,85 @@ if isempty(D)
 	warning('%s: D is empty???!!!??!!', mfilename);
 	return
 end
+% local copy of sample rate
+Fs = Dinf.indev.Fs;
 
 %---------------------------------------------------------------------
 % get info from filename - this makes some assumptions about file
 % name structure!
 % <animal id #>_<date>_<penetration #>_<unit #>_<other info>.dat
 %---------------------------------------------------------------------
-[animal, datecode, penetration, unit, other] = opto_name_deconstruct(datafile);
+[fname, animal, datecode, penetration, unit, other] = opto_name_deconstruct(datafile);
 
+%---------------------------------------------------------------------
+% determine global RMS and max - used for thresholding
+%---------------------------------------------------------------------
+% first, get  # of stimuli (called ntrials by opto) as well as # of reps
+nstim = Dinf.test.nCombinations;
+nreps = Dinf.test.Reps;
+
+% allocate matrices
+netrmsvals = zeros(nstim, nreps);
+maxvals = zeros(nstim, nreps);
+% find rms, max vals for each stim
+for s = 1:nstim
+	netrmsvals(s, :) = rms(tracesByStim{s});
+	maxvals(s, :) = max(abs(tracesByStim{s}));
+end
+% compute overall mean rms for threshold
+fprintf('Calculating mean and max RMS for data...\n');
+mean_rms = mean(reshape(netrmsvals, numel(netrmsvals), 1));
+fprintf('\tMean rms: %.4f\n', mean_rms);
+% find global max value (will be used for plotting)
+global_max = max(max(maxvals));
+fprintf('\tGlobal max abs value: %.4f\n', global_max);
+
+%---------------------------------------------------------------------
+% Some test-specific things... (removed unneeded types from original 
+% code in optoproc
+%---------------------------------------------------------------------
+switch upper(Dinf.test.Type)
+	case 'WAVFILE'
+		% get list of stimuli (wav file names)
+		varlist = Dinf.test.wavlist;
+		nvars = length(varlist);
+		titleString = cell(nvars, 1);
+		for v = 1:nvars
+			if v == 1 
+				titleString{v} = {fname, sprintf('wav name: %s', varlist{v})};
+			else
+				titleString{v} = sprintf('wav name: %s', varlist{v});
+			end
+		end
+	otherwise
+		error('%s: unsupported test type %s', mfilename, Dinf.test.Type);
+end
+
+%---------------------------------------------------------------------
+%% find spikes!
+%---------------------------------------------------------------------
+% local copy of sample rate
+Fs = Dinf.indev.Fs;
+
+% different approaches to storage depending on test type
+switch upper(Dinf.test.Type)
+	case 'FREQ+LEVEL'
+		% for FRA data, nvars has values [nfreqs nlevels];
+		spiketimes = cell(nvars(2), nvars(1));
+		for v1 = 1:nvars(1)
+			for v2 = 1:nvars(2)
+				% use rms threshold to find spikes
+				spiketimes{v2, v1} = ...
+						spikeschmitt2(tracesByStim{v2, v1}', Threshold*mean_rms, ...
+																			1, Fs, 'ms');
+			end
+		end
+	otherwise
+		% if test is not FREQ+LEVEL (FRA), nvars will be a single number
+		spiketimes = cell(nvars, 1);
+		for v = 1:nvars
+			% use rms threshold to find spikes
+			spiketimes{v} = spikeschmitt2(tracesByStim{v}', Threshold*mean_rms, ...
+																				1, Fs, 'ms');
+		end
+end
