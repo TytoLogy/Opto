ecc1bf5abb4dfacdc46b9e68e2a3c7da0dce8ff4
sshanbhag <sshanbhag@neomed.edu>
2020-07-01 20:50:44 -0400

    deleted tmp script dir

--- Scripts/tmp/MTwav_standalone.m
+++
@@ -1,8 +0,0 @@
-%-------------------------------------------------------------------------
-% Standalone script
-%-------------------------------------------------------------------------
-% indicate that this is a standalone script
-test.Type = 'STANDALONE';
-test.Name = 'WAV';
-% test.Function = @wav;
-test.Function = @MTwav;
